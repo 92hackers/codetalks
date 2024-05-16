c92f573aee3d5b61e3d49270e6a60781280feaec
chenyuan <cy476571@gmail.com>
2024-05-16 11:25:19 +0800

    skip vcs dir

--- internal/scanner/scan.go
+++ internal/scanner/scan.go
@@ -11,7 +11,18 @@ import (
 	"io/fs"
 	"log"
 	"path/filepath"
+	"strings"
 )
+
+func isVCSDir(path string) bool {
+	vcsDirs := []string{".git", ".svn", ".hg", ".bzr", ".cvs"}
+	for _, dir := range vcsDirs {
+		if strings.Contains(path, dir) {
+			return true
+		}
+	}
+	return false
+}
 
 func handler(path string, d fs.DirEntry, err error) error {
 	if err != nil {
@@ -21,6 +32,14 @@ func handler(path string, d fs.DirEntry, err error) error {
 	// dir
 	if d.IsDir() {
 		fmt.Println("dir:", path)
+
+		// Skip VCS directories
+		_, dir := filepath.Split(path)
+		fmt.Println("last segment:", dir)
+		if isVCSDir(dir) {
+			return fs.SkipDir
+		}
+
 		return nil
 	}
 
